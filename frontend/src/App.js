d5cd5b8633f5505351ae9e433ef0cf7e87a8517c
Kishan Patel <kspatel2018@gmail.com>
2022-10-25 18:31:10 -0700

    implement react routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import './App.css';
 import Header from './components/Header'
 import NotesListPage from "./pages/NotesListPage";
 
+
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <NotesListPage/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" exact element={<NotesListPage/>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
